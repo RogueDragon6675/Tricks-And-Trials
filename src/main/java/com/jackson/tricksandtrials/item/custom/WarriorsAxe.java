57977c5eda1b0f2907a5d2b9a73b113519f2bcb3
RogueDragon6675 <jacksonbrogan14@gmail.com>
2026-03-28 12:33:14 -0400

    girg

--- src/main/java/com/jackson/tricksandtrials/item/custom/WarriorsAxe.java
+++ src/main/java/com/jackson/tricksandtrials/item/custom/WarriorsAxe.java
@@ -1,24 +1,57 @@
 package com.jackson.tricksandtrials.item.custom;
 
+import com.jackson.tricksandtrials.TricksandTrials;
+import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+
+
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.monster.Evoker;
+import net.minecraft.world.entity.player.Player;
+
 import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 public class WarriorsAxe extends AxeItem {
+    float shockwaveSpeed = 1.f;
+    float shockwaveScaling = 0.4f;
+    float shockwaveDamage = 6f;
+    float circleShockwaveDuration = 0.8f;
+    float lineShockwaveDuration = 30f;
+    int numCircleProjectiles = 16;
     public WarriorsAxe(Properties p_40524_,Tier p_40521_ ) {
         super(p_40521_, p_40524_);
     }
 
     @Override
-    public InteractionResult useOn(UseOnContext context) {
-        return super.useOn(context);
-    }
-    public void createSonicBoom()
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+    if (player.isShiftKeyDown())
     {
-        Evoker
+    createCircleBoom();
+    }else {
+        createLineBoom();
+    }
+
+
+        return super.use(level, player, usedHand);
+    }
+
+    public void createCircleBoom()
+    {
+     TricksandTrials.LOGGER.info("DidCircleATTACK");
+    }
+    public void createLineBoom()
+    {
+        TricksandTrials.LOGGER.info("DidLineATTACK");
+    }
+    private void createBoomEntity(Vec3 direction, float lifetime)
+    {
 
     }
-    
+
 }
